e212484371a448b3a3128c337cc892b82c86c335
Mohamed Kelany <mohamed.kelany@outlook.com>
2022-09-29 07:50:13 +0200

    add App Center scripts

---
+++ App Center/appcenter-post-clone.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Example: Clone a required repository
+git clone https://github.com/moKelani/CICDScripts.git
+
+# Example: Install App Center CLI
+npm install -g appcenter-cli
+
+# Install CocoaPods using Homebrew.
+ brew install cocoapods
+
+ # Install dependencies you manage with CocoaPods.
+ pod install
